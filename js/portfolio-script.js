6a748f919f1016c3a1aae688a717984a9973f573
Lisa Jurries <lisajurries@live.com>
2021-08-10 09:18:47 -0700

    Fixes

--- js/portfolio-script.js
+++ js/portfolio-script.js
@@ -4,7 +4,7 @@ fetch('JSON/JSONforPortfolio-cards.json')
 
         const section = document.querySelector('#section')
 
-        data.section.forEach(item => {
+        data.type.forEach(item => {
 
             console.log(data)
             let cards = document.querySelector('#cards')
@@ -18,19 +18,19 @@ fetch('JSON/JSONforPortfolio-cards.json')
 
             // //card image
             let img = document.createElement('img')
-            img.setAttribute('src', type.image)
+            img.setAttribute('src', item.type.[0].image)
             img.classList.add('card-img-top')
             img.classList.add('img-fluid')
-            section.appendChild(img)
+            cards.appendChild(img)
 
             //card inner div
             let div = document.createElement('div')
             div.classList.add('card-body')
-            div.append(div)
+            cards.append(div)
 
             //h5 in inner div
             let h5 = document.createElement('h5')
-            h5.innerText = story.title
+            h5.innerText = item.type[0].title
             h5.classList.add('fw-bold')
             h5.classList.add('card-title')
             div.appendChild(h5)
@@ -38,13 +38,13 @@ fetch('JSON/JSONforPortfolio-cards.json')
             //p in inner div
             let p = document.createElement('p')
             p.classList.add('card-text')
-            p.innerHTML = `<div class="mt-2"> ${type.exerpt} </div>`
+            p.innerHTML = `<div class="mt-2"> ${item.type[0].exerpt} </div>`
             div.appendChild(p)
 
             //button in inner div
             let a = document.createElement('a')
             a.innerText = 'Read More'
-            a.setAttribute('href', type.link)
+            a.setAttribute('href', item.type[0].link)
             a.classList.add('btn')
             a.classList.add('btn-dark')
             div.appendChild(a)
